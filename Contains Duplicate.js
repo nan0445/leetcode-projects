54da2cdbde156c98a31c3ac85bbc95e1c2ffe5e7
nan0445 <31909450+nan0445@users.noreply.github.com>
2017-09-21 12:06:20 -0400

    Create Contains Duplicate.js

---
+++ Contains Duplicate.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+    nums = nums.sort();
+    for (i=0;i<nums.length-1; i++) {
+        if (nums[i] === nums[i+1]) return true;
+    }
+    return false;
+};
