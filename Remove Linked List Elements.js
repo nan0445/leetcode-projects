ac8cfdd54390f97afe7c3c76ba0e939b23782a14
nan0445 <31909450+nan0445@users.noreply.github.com>
2017-09-26 22:29:32 -0400

    Create Remove Linked List Elements.js

---
+++ Remove Linked List Elements.js
@@ -0,0 +1,19 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} val
+ * @return {ListNode}
+ */
+var removeElements = function(head, val) {
+    
+    if (head === null) return null;
+    if (head.val === val) return removeElements(head.next, val);
+    else head.next = removeElements(head.next, val);
+    return head;
+};
