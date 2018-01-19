b24340f378ba004379a77bd75aa8d16d586fa97a
nan0445 <31909450+nan0445@users.noreply.github.com>
2018-01-18 23:13:10 -0500

    Create Evaluate Reverse Polish Notation.js

---
+++ Evaluate Reverse Polish Notation.js
@@ -0,0 +1,29 @@
+/**
+ * @param {string[]} tokens
+ * @return {number}
+ */
+var evalRPN = function(tokens) {
+    var temp = [];
+    for (let i=0; i<tokens.length; i++) {
+        if (tokens[i]!=="+" && tokens[i]!=="-" && tokens[i]!=="*" && tokens[i]!=="/") {temp.push(tokens[i]-0); res.push(tokens[i])}
+        else if (tokens[i]==="+") {
+            var x2 = temp.pop();
+            var x1 = temp.pop();
+            temp.push(x1+x2);
+        } else if (tokens[i]==="-") {
+            var x2 = temp.pop();
+            var x1 = temp.pop();
+            temp.push(x1-x2);
+        } else if (tokens[i]==="*") {
+            var x2 = temp.pop();
+            var x1 = temp.pop();
+            temp.push(x1*x2);
+        } else if (tokens[i]==="/") {
+            var x2 = temp.pop();
+            var x1= temp.pop();
+            if (x1*x2<0) temp.push(-Math.floor(-x1/x2));
+            else temp.push(Math.floor(x1/x2));
+        }
+    }
+    return temp.pop();
+};
